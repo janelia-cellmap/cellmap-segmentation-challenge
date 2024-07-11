809967a1f583ba7a9945d4800da4b0c5342bc618
rhoadesScholar <rhoadesj@hhmi.org>
2024-07-11 11:00:16 -0400

    feat: 🎉 Make 2D training script.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 # CellMap Segmentation Challenge
 Repository of scripts to facilitate participation in CellMap's segmentation challenge. This includes downloading data, simple setups for training 2D and 3D models, workflows for prediction and post-processing on out-of-memory arrays, and evaluation of results against validation data.
+
+# Getting started
+
+## Set up your environment
+
+We recommend using micromamba to create a new environment with the required dependencies. You can install micromamba by following the instructions [here](https://mamba.readthedocs.io/en/latest/installation/micromamba-installation.html#automatic-install). Once you have micromamba installed, you can create a new environment with the required dependencies by running the following commands:
+
+```bash
+micromamba create -n cellmap-segmentation-challenge python=3.10 pytorch torchvision numpy tqdm cellmap-data -c pytorch -c conda-forge
---
+++ train_2D.py
@@ -0,0 +1,128 @@
+# %% Imports
+import torch
+import numpy as np
+from tqdm import tqdm, trange
+from utils.dataloader import get_dataloader
+from utils.dataloader import get_result_figs
+
+# %% Set hyperparameters and other configurations
+learning_rate = 0.0001  # learning rate for the optimizer
+batch_size = 32  # batch size for the dataloader
+epochs = 10  # number of epochs to train the model for
+iterations_per_epoch = 1000  # number of iterations per epoch
+num_workers = 4  # number of workers for the dataloader to use
+random_seed = 42  # random seed for reproducibility
+init_model_features = 32  # number of initial features for the model
+
+classes = ["nuc"]  # list of classes to segment
+model_name = "2D_unet"  # name of the model to use
+data_base_path = "data"  # base path where the data is stored
+figures_save_path = (
+    "figures/{model_name}_{epoch}.png"  # path to save the example figures
+)
+model_save_path = (
+    "checkpoints/{model_name}_{epoch}.pth"  # path to save the model checkpoints
+)
+datasplit_path = "datasplit.csv"  # path to the datasplit file that defines the train/val split the dataloader should use
+
+# %% Set the random seed
+torch.manual_seed(random_seed)
+np.random.seed(random_seed)
+
+# %% Check that the GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# %% Download the data and make the dataloader
+train_loader, val_loader = get_dataloader(
+    datasplit_path,
+    batch_size=batch_size,
+    iterations_per_epoch=iterations_per_epoch,
+    num_workers=num_workers,
+    device=device,
+)
+
+# %% Define the model
+model = torch.hub.load(
+    "mateuszbuda/brain-segmentation-pytorch",
+    "unet",
+    in_channels=1,
+    out_channels=len(classes),
+    init_features=init_model_features,
+    pretrained=False,
+)
+
+model = model.to(device)
+
+# %% Define the optimizer
+optimizer = torch.optim.RAdam(model.parameters(), lr=learning_rate)
+
+# %% Define the loss function
+criterion = torch.nn.CrossEntropyLoss()
+
+# %% Train the model
+losses = np.empty((epochs * iterations_per_epoch))
+validation_scores = np.empty(epochs)
+
+# Training outer loop, across epochs
+training_bar = trange(epochs, leave=True, position=0)
+for epoch in training_bar:
+
+    # Set the model to training mode to enable backpropagation
+    model.train()
+
+    # Training loop for the epoch
+    epoch_bar = tqdm(train_loader, leave=False, position=1)
+    for i, (inputs, labels) in enumerate(epoch_bar):
+
+        # Zero the gradients, so that they don't accumulate across iterations
+        optimizer.zero_grad()
+
+        # Forward pass (compute the output of the model)
+        outputs = model(inputs)
+
+        # Compute the loss
+        loss = criterion(outputs, labels)
+
+        # Save the loss for logging
+        losses[epoch * iterations_per_epoch + i] = loss.item()
+
+        # Backward pass (compute the gradients)
+        loss.backward()
+
+        # Update the weights
+        optimizer.step()
+
+        # Update the progress bar
+        epoch_bar.set_description(f"Loss: {loss.item():.4f}")
+
+    # Save the model
+    torch.save(
+        model.state_dict(),
+        model_save_path.format(epoch=epoch + 1, model_name=model_name),
+    )
+
+    # Set the model to evaluation mode to disable backpropagation
+    model.eval()
+
+    # Compute the validation score by averaging the loss across the validation set
+    val_score = 0
+    val_bar = tqdm(val_loader, leave=False, position=1)
+    for inputs, labels in val_bar:
+        outputs = model(inputs)
+        val_score += criterion(outputs, labels).item()
+    val_score /= len(val_loader)
+    validation_scores[epoch] = val_score
+
+    # Update the progress bar
+    training_bar.set_description(f"Validation score: {val_score:.4f}")
+    training_bar.refresh()
+
+    # Generate and save some example figures from the validation set
+    get_result_figs(
+        inputs,
+        outputs,
+        labels,
+        classes,
+        figures_save_path.format(epoch=epoch + 1, model_name=model_name),
+    )
