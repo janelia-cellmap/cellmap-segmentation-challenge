33dc57d985ef5a0ddc6202a0ec60e2e8f039610c
aemmav <aemmav@icloud.com>
2024-07-25 14:17:03 -0400

    add comments for new method

--- utils/model_load.py
+++ utils/model_load.py
@@ -1,16 +1,19 @@
+# Imports
 import os
 import torch
 import glob
 
 
+# load_latest checks to see if there are any checkpoints to load in
 def load_latest(search_path, model):
+    # Check if there are any files matching the checkpoint save path
     checkpoint_files = glob.glob(search_path)
     if checkpoint_files:
-        checkpoint_files.sort(
-            key=os.path.getmtime, reverse=True
-        )  # If there are checkpoints they are sorted by modification time
+        # If there are checkpoints, sort by modification time
+        checkpoint_files.sort(key=os.path.getmtime, reverse=True)
+        # Saves the latest checkpoint into a variable
         newest_checkpoint = checkpoint_files[0]
 
-        # Loads the most recent checkpoint into the model
+        # Loads the most recent checkpoint into the model and prints the file path
         model.load_state_dict(torch.load(newest_checkpoint))
         print(f"Loaded latest checkpoint: {newest_checkpoint}")
