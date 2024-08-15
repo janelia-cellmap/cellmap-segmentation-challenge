9b6dec5bfcf750adf1bec1ef4e37cab7add45f71
rhoadesScholar <rhoadesj@hhmi.org>
2024-08-14 23:11:36 -0400

    feat(Evaluation): 🚧 Evaluation loading and score calculation ready for testing.

--- src/cellmap_segmentation_challenge/utils/evaluate.py
+++ src/cellmap_segmentation_challenge/utils/evaluate.py
@@ -1,7 +1,62 @@
 # %%
+import re
 import numpy as np
 from scipy import ndimage
+import tensorstore as ts
+from upath import UPath
+import h5py
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 import matplotlib.pyplot as plt
+
+TRUTH_DATASETS = {
+    "truth_1": "s3://truth_1.zarr/{label}/{resolution_level}",
+    "truth_2": "s3://truth_2.zarr/{label}/{resolution_level}",
+}
+
+METRICS = [
+    "false_positives",
+    "false_negatives",
+    "false_positive_stats",
+    "false_negative_stats",
+    "iou",
+    "class_score",
+]
+
+RESOLUTION_LEVELS = {
+    8: "s0",
+    16: "s1",
+    32: "s2",
+    64: "s3",
+    128: "s4",
+    256: "s5",
+    512: "s6",
+    1024: "s7",
+    2048: "s8",
+}
+
+SUMMARY_METRICS = ["false_positives", "false_negatives", "iou", "class_score"]
+
+
+def _zarr_spec_from_path(path: str) -> ...:
+    if re.match(r"\w+\://", path):  # path is a URI
+        kv_store = path
+    else:
+        kv_store = {"driver": "file", "path": path}
+    return {"driver": "zarr", "kvstore": kv_store}
+
+
+def load_data(path: str):
+    assert UPath(path).exists()
+    if ".zarr" in path:
+        spec = _zarr_spec_from_path(path)
+        return np.array(ts.open(spec, read=True, write=False).result())
+    elif ".h5" in path or ".hdf5" in path:
+        with h5py.File(path, "r") as f:
+            return f["data"][:]
 
 
 class ClassError:
@@ -12,10 +67,18 @@ class ClassError:
     - Intersection over Union (IoU)
     - False positive distance statistics (mean, std, max, count, median)
     - False negative distance statistics (mean, std, max, count, median)
+    Functions were adapted from https://github.com/cremi/cremi_python
     This class is used by the TestScore class to calculate the error across all test data.
     """
 
-    def __init__(self, resolution, test, truth, distance_max=80, norm_slope=1):
+    def __init__(
+        self,
+        resolution: tuple,
+        test: np.ndarray,
+        truth: np.ndarray,
+        distance_max: float = 80.0,
+        norm_slope: float = 1.0,
+    ):
         """
         Parameters
         ----------
@@ -37,6 +100,25 @@ class ClassError:
         self.test = test
         self.truth = truth
 
+        if self.test.shape != self.truth.shape:
+            # Crop the test data to the shape of the ground truth data
+            truth_shape = np.array(truth.shape)
+            test_shape = np.array(test.shape)
+            if any(test_shape > truth_shape):
+                logger.warning(
+                    "Test data is larger than the ground truth data. Cropping to the shape of the ground truth data - assumes the ground truth data is centered in the test data."
+                )
+                crop = (test_shape - truth_shape) // 2
+                self.test = self.test[
+                    crop[0] : crop[0] + truth.shape[0],
+                    crop[1] : crop[1] + truth.shape[1],
+                    crop[2] : crop[2] + truth.shape[2],
+                ]
+            else:
+                raise ValueError(
+                    "Test data must be the same shape as the ground truth data, or larger."
+                )
+
         self.test_edt = self.normalize_distances(
             ndimage.distance_transform_edt(self.test, sampling=self.resolution)
         )
@@ -54,60 +136,154 @@ class ClassError:
         # Clipped to between 0 and 1, scaled to distance_max
         # return np.clip(x, 0, self.distance_max) / self.distance_max
 
-    def count_false_positives(self, threshold=40):
-
+    def false_positives(self, threshold=40):
         mask1 = self.test == 0
         mask2 = self.truth_edt > threshold
         false_positives = self.truth_edt[np.logical_and(mask1, mask2)]
 
         return false_positives.size
 
-    def count_false_negatives(self, threshold=40):
-
+    def false_negatives(self, threshold=40):
         mask1 = self.truth == 0
         mask2 = self.test_edt > threshold
         false_negatives = self.test_edt[np.logical_and(mask1, mask2)]
 
         return false_negatives.size
 
-    def acc_false_positives(self):
+    def false_positive_stats(self):
+        if hasattr(self, "_false_positive_stats"):
+            return self._false_positive_stats
+        else:
+            mask = self.test == 0
+            false_positives = self.truth_edt[mask]
+            self._false_positive_stats = {
+                "mean": np.mean(false_positives),
+                "std": np.std(false_positives),
+                "max": np.amax(false_positives),
+                "count": false_positives.size,
+                "median": np.median(false_positives),
+            }
+        return self._false_positive_stats
 
-        mask = self.test == 0
-        false_positives = self.truth_edt[mask]
-        stats = {
-            "mean": np.mean(false_positives),
-            "std": np.std(false_positives),
-            "max": np.amax(false_positives),
-            "count": false_positives.size,
-            "median": np.median(false_positives),
-        }
-        return stats
-
-    def acc_false_negatives(self):
-
-        mask = self.truth == 0
-        false_negatives = self.test_edt[mask]
-        stats = {
-            "mean": np.mean(false_negatives),
-            "std": np.std(false_negatives),
-            "max": np.amax(false_negatives),
-            "count": false_negatives.size,
-            "median": np.median(false_negatives),
-        }
-        return stats
+    def false_negative_stats(self):
+        if hasattr(self, "_false_negative_stats"):
+            return self._false_negative_stats
+        else:
+            mask = self.truth == 0
+            false_negatives = self.test_edt[mask]
+            self._false_negative_stats = {
+                "mean": np.mean(false_negatives),
+                "std": np.std(false_negatives),
+                "max": np.amax(false_negatives),
+                "count": false_negatives.size,
+                "median": np.median(false_negatives),
+            }
+        return self._false_negative_stats
 
     def iou(self):
+        if hasattr(self, "_iou"):
+            return self._iou
+        else:
+            intersection = np.logical_and(self.test, self.truth)
+            union = np.logical_or(self.test, self.truth)
+            self._iou = np.sum(intersection) / np.sum(union)
+        return self._iou
 
-        intersection = np.logical_and(self.test, self.truth)
-        union = np.logical_or(self.test, self.truth)
+    def class_score(self):
+        """
+        This function returns a summary score for each class per dataset, for easy agglomeration across datasets and classes.
+        """
+        if hasattr(self, "_class_score"):
+            return self._class_score
+        else:
+            self._class_score = self.iou
+        return self._class_score
 
-        iou = np.sum(intersection) / np.sum(union)
-
-        return iou
+    def return_results(self, metrics=METRICS):
+        return {k: getattr(self, k)() for k in metrics}
 
 
 # %%
-class TestScore:
-    def __init__(
-        self, labels, resolution, test_datasets, distance_max=80, norm_slope=1
-    ): ...
+def score(
+    label_dict: dict[str, int],
+    test_datasets: dict[str, str],
+    resolution: int,
+    distance_max: float = 80.0,
+    norm_slope: float = 1.0,
+    truth_datasets: dict[str, str] = TRUTH_DATASETS,
+):
+    """
+    Parameters
+    ----------
+    label_dict : dict[str, int]
+        A dictionary mapping class labels to their index in the test_datasets segmentation.
+    test_datasets : dict[str, str]
+        A dictionary mapping dataset names to their paths.
+    resolution : int
+        The resolution of the data in nanometers per pixel. This should be one of the following: 8, 16, 32, 64, 128, 256, 512, 1024, 2048. Data should be isotropic.
+    distance_max : float, optional
+        The maximum distance to consider for distance-based metrics, by default 80.
+    norm_slope : float, optional
+        The slope of the tanh function used to normalize distances, by default 1.
+    truth_datasets : dict[str, str], optional
+        A dictionary mapping dataset names to their ground truth paths, by default TRUTH_DATASETS. Dataset names must match those in test_datasets, and are expected to have the format: `provided_path/{label}/{resolution_level}`.
+
+    Returns
+    -------
+    float
+        The overall score across all classes and datasets.
+    dict
+        A dictionary mapping class labels to dictionaries of summary metrics.
+    dict
+        A dictionary mapping dataset names to dictionaries of class metrics.
+    """
+    resolution_level = RESOLUTION_LEVELS[resolution]
+    resolution = (resolution, resolution, resolution)
+    dataset_results = {k: {} for k in test_datasets.keys()}
+    for dataset, path in test_datasets.items():
+        test = load_data(path)
+        for label, idx in label_dict.items():
+            try:
+                truth = load_data(
+                    truth_datasets[dataset].format(
+                        label=label, resolution_level=resolution_level
+                    )
+                )
+            except:
+                logger.error(
+                    f"Could not load groundtruth data for {label} in {dataset}"
+                )
+                continue
+            class_error = ClassError(
+                resolution, test[idx], truth, distance_max, norm_slope
+            )
+            dataset_results[dataset][label] = class_error.return_results()
+
+    dataset_results = dataset_results
+
+    summary_results = {k: {} for k in label_dict.keys()}
+    overall_score = []
+    for label in label_dict.keys():
+        for metric in SUMMARY_METRICS:
+            summary_results[label][metric] = np.mean(
+                [
+                    dataset_results[dataset][label][metric]
+                    for dataset in test_datasets.keys()
+                ]
+            )
+        overall_score.append(summary_results[label]["class_score"])
+
+    summary_results = summary_results
+    overall_score = np.mean(overall_score)
+
+    return overall_score, summary_results, dataset_results
+
+
+def get_leaderboard_stats(summary_results):
+    leaderboard_stats = {}
+    for label, stats in summary_results.items():
+        leaderboard_stats[label] = stats["class_score"]
+    return leaderboard_stats
+
+
+# %%
