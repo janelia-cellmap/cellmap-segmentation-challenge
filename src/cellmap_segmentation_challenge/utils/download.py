b6e99cfbda60798f8701859fe4e06319e04e37a8
rhoadesScholar <rhoadesj@hhmi.org>
2024-08-14 23:25:28 -0400

    chore(Download): 🚧 Further specify download script.

--- src/cellmap_segmentation_challenge/utils/download.py
+++ src/cellmap_segmentation_challenge/utils/download.py
@@ -1,17 +1,35 @@
 import os
 from typing import Sequence
 
+from .shared import TRUTH_DATASETS, RESOLUTION_LEVELS
 
-def download(classes: Sequence[str], min_scale: int, output_path: str | os.PathLike):
+
+def download(
+    classes: Sequence[str],
+    resolution: int,
+    output_path: str | os.PathLike,
+    padding: Sequence[int] = (0, 0, 0),
+    fill_value: float = 0,
+):
     """Download the dataset for the CellMap Segmentation Challenge.
 
     Args:
-        classes: The classes to download.
-        min_scale: The minimum scale of the images to download.
-        output_path: The path to the directory where to save the dataset.
+        classes: Sequence[str]
+            The label classes to download. e.g. ["cell", "nuc", "mito"]
+        resolution : int
+            The resolution of the data in nanometers per voxel. This should be one of the following: 8, 16, 32, 64, 128, 256, 512, 1024, 2048. Data should be isotropic.
+        output_path: str | os.PathLike
+            The path to the directory where to save the dataset.
+        padding: Sequence[int], optional
+            The amount of padding around ground truth volumes to add to raw data, in voxels per axis. Default is (0, 0, 0)
+        fill_value: float, optional
+            The value to fill the padding with if it extends past the edge of the available raw data. Default is 0.
     """
+
+    resolution_level = RESOLUTION_LEVELS[resolution]
+
     # Download the dataset
-    pass
+    ...
 
     # Extract the dataset
-    pass
+    ...
