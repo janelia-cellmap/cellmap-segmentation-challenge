af02c860988586332a7326b9fafec1ee4fb07682
rhoadesScholar <rhoadesj@hhmi.org>
2024-09-10 14:58:26 -0400

    Update volume shape and optimize evaluation functions

--- src/cellmap_segmentation_challenge/utils/benchmark/evaluate.py
+++ src/cellmap_segmentation_challenge/utils/benchmark/evaluate.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # %% First make a GT array
 
 # SET SHAPE AND NUMBER OF POINTS HERE
-shape = (256, 256, 256) # (z, y, x) shape of the volume
+shape = (128, 128, 128) # (z, y, x) shape of the volume
 num_points = [shape[0] // 10, shape[0] // 5] # number of random objects in the volume [min, max]
 # =====================================
 
@@ -36,6 +36,8 @@ print(f"Shape of volume: {array.shape}\nNumber of IDs: {len(np.unique(array))}")
 # Euclidean distance transform on each ID separately
 def test(array):
     for id in np.unique(array):
+        if id == 0:
+            continue
         this = edt(array==id)
 
 %timeit test(array)
@@ -45,21 +47,44 @@ def test(array):
 def h_test(array):
     ids = np.unique(array)
     for i, i1 in enumerate(ids):
+        if i1 == 0:
+            continue
         for j, i2 in enumerate(ids):
             if j<=i:
                 continue
             this = hausdorff_distance(array==i1, array==i2)
 
 %timeit h_test(array)
+
+# %% 
+# Pairwise Jaccard distance between overlapping labels
+def j_test_fast(array1, array2):
+    mean_overlaps = []
+    ids1 = np.unique(array1)
+    for i1 in ids1:
+        if i1 == 0:
+            continue
+        mask = array1==i1
+        ids2 = list(np.unique(array2 * mask))
+        ids2.remove(0)
+        mean_overlaps.append(len(ids2))
+        for i2 in ids2:
+            this = jaccard_score(mask.flatten(), (array2==i2).flatten())
+    # print(f"Mean # of overlapping IDs: {np.mean(mean_overlaps)}")
+%timeit j_test_fast(array, array)
+
 # %%
-# Pairwise Jaccard distance between individual labels
+# Pairwise Jaccard distance between all individual labels
 def j_test(array):
     ids = np.unique(array)
     for i, i1 in enumerate(ids):
+        if i1 == 0:
+            continue
         for j, i2 in enumerate(ids):
             if j<=i:
                 continue
             this = jaccard_score(array.flatten()==i1, array.flatten()==i2)
 
 %timeit j_test(array)
+
 # %%
