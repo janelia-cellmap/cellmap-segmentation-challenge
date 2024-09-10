78b7a2e0c1d7e1a5e25ec1628e4cd3345fe7cd1c
rhoadesScholar <rhoadesj@hhmi.org>
2024-09-09 22:39:48 -0400

    test(Evaluation): 🧐 Add bookmarking scripts for evaluation.

---
+++ src/cellmap_segmentation_challenge/utils/benchmark/evaluate.py
@@ -0,0 +1,65 @@
+# %%
+import numpy as np
+import gunpowder as gp
+from scipy.ndimage import distance_transform_edt as edt
+from skimage.metrics import hausdorff_distance
+from sklearn.metrics import jaccard_score
+from pipeline import random_source_pipeline
+import matplotlib.pyplot as plt
+
+# %% First make a GT array
+
+# SET SHAPE AND NUMBER OF POINTS HERE
+shape = (128, 128, 128) # (z, y, x) shape of the volume
+num_points = [shape[0] // 10, shape[0] // 5] # number of random objects in the volume [min, max]
+# =====================================
+
+src, req = random_source_pipeline(shape=shape, num_points=num_points)
+
+with gp.build(src):
+    batch = src.request_batch(req)
+    array = list(batch.arrays.values())[0].data
+
+
+# %%
+# Plot the GT array and print some info
+plt.imshow(array[shape[0]//2], cmap='tab20')
+plt.colorbar()
+
+print(f"Shape of volume: {array.shape}\nNumber of IDs: {len(np.unique(array))}")
+
+# %%
+# Euclidean distance transform on all IDs
+%timeit edt(array)
+
+# %%
+# Euclidean distance transform on each ID separately
+def test(array):
+    for id in np.unique(array):
+        this = edt(array==id)
+
+%timeit test(array)
+
+# %%
+# Pairwise Haussdorf distance between individual labels
+def h_test(array):
+    ids = np.unique(array)
+    for i, i1 in enumerate(ids):
+        for j, i2 in enumerate(ids):
+            if j<=i:
+                continue
+            this = hausdorff_distance(array==i1, array==i2)
+
+%timeit h_test(array)
+# %%
+# Pairwise Jaccard distance between individual labels
+def j_test(array):
+    ids = np.unique(array)
+    for i, i1 in enumerate(ids):
+        for j, i2 in enumerate(ids):
+            if j<=i:
+                continue
+            this = jaccard_score(array.flatten()==i1, array.flatten()==i2)
+
+%timeit j_test(array)
+# %%
--- src/cellmap_segmentation_challenge/utils/evaluate.py
+++ src/cellmap_segmentation_challenge/utils/evaluate.py
@@ -11,7 +11,7 @@ import fsspec
 import requests
 import tempfile
 
-from shared import TRUTH_DATASETS, RESOLUTION_LEVELS, CLASS_DATASETS
+from .shared import TRUTH_DATASETS, RESOLUTION_LEVELS, CLASS_DATASETS
 
 import logging
 
@@ -400,35 +400,5 @@ def print_leaderboard_stats(leaderboard_stats):
         print(f"{label}: {stats['mean']} +/- {stats['std']}")
     print(f"Overall: {np.mean([v['mean'] for v in leaderboard_stats.values()])}")
 
-
-# %%
-# Example usage ===========================================================
-import matplotlib.pyplot as plt
-
-label_dict = {"mito": 0}
-test_datasets = {
-    "jrc_hela-2": "/nrs/cellmap/bennettd/data/jrc_hela-2/jrc_hela-2.zarr/recon-1/labels/groundtruth/crop155/mito/s0"
-}
-resolution = 8
-distance_max = 80.0
-norm_slope = 1.0
-truth_datasets = {
-    "jrc_hela-2": "/nrs/cellmap/bennettd/data/jrc_hela-2/jrc_hela-2.zarr/recon-1/labels/groundtruth/crop155/{label}/{resolution_level}"
-}
-overall_score, summary_results, dataset_results = score(
-    label_dict, test_datasets, resolution, distance_max, norm_slope, truth_datasets
-)
-
-# %%
-# leaderboard_stats = get_leaderboard_stats(
-#     label_dict, test_datasets, resolution, distance_max, norm_slope, truth_datasets
-# )
-CLASS_DATASETS = {"mito": ["jrc_hela-2"]}
-leaderboard_stats = {k: {} for k in label_dict.keys()}
-for label in label_dict.keys():
-    leaderboard_stats[label]["mean"], leaderboard_stats[label]["std"] = (
-        get_leaderboard_stat(label, dataset_results)
-    )
-print_leaderboard_stats(leaderboard_stats)
 
 # %%
