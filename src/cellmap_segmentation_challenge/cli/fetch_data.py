7da81905695cc9b4d3cc007a10f2956b9eb821c7
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-09-17 12:26:44 -0400

    fix: handle null crop_multiscale_group

--- src/cellmap_segmentation_challenge/cli/fetch_data.py
+++ src/cellmap_segmentation_challenge/cli/fetch_data.py
@@ -125,41 +125,43 @@ def fetch_crops_cli(crops: str, raw_padding: str, dest_dir: str, access_mode: st
                     break
                 except (ValueError, TypeError):
                     continue
-                raise ValueError(f'No multiscale groups found in {gt_source_url}. No EM data can be fetched.')
-            
-            em_group_inventory = ()
-            em_source_arrays_sorted = sorted(em_source_arrays.items(), key=lambda kv: np.prod(kv[1].shape), reverse=True)
-            gt_source_arrays_sorted = sorted(crop_multiscale_group.items(), key=lambda kv: np.prod(kv[1].shape), reverse=True)
-            
-            # apply padding in a resolution-aware way
-            _, (base_em_scale, _) = transforms_from_coords(em_source_arrays_sorted[0][1].coords, transform_precision=4)
-            _, (base_gt_scale, _) = transforms_from_coords(gt_source_arrays_sorted[0][1].coords, transform_precision=4)
 
-            for key, array in em_source_arrays_sorted:
-                _, (current_scale, _) = transforms_from_coords(array.coords, transform_precision=4)
-                if fetch_all_em_resolutions: ratio_threshold = 0
-                else: ratio_threshold = 0.9
-                scale_ratios = tuple(s_current / s_gt for s_current, s_gt in zip(current_scale.scale, base_gt_scale.scale))
-                if all(tuple(x > ratio_threshold for x in scale_ratios)):
-                    
-                    relative_scale = base_em_scale.scale[0] / current_scale.scale[0]
-                    current_pad = int(padding * relative_scale)
-                    slices = subset_to_slice(array, crop_multiscale_group['s0'])
-                    slices_padded = tuple(slice(max(sl.start - current_pad, 0), min(sl.stop + current_pad, shape), sl.step) for sl, shape in zip(slices, array.shape))
-                    new_chunks = tuple(map(lambda v: '/'.join([key, v]), get_chunk_keys(em_source_group[key], slices_padded)))
-                    log.info(f'Gathering {len(new_chunks)} chunks from level {key}.')
-                    em_group_inventory += new_chunks
-                else:
-                    log.info(f'Skipping scale level {key} because it is sampled more densely than the groundtruth data')
+            if crop_multiscale_group is None:
+                log.info(f'No multiscale groups found in {gt_source_url}. No EM data can be fetched.')
+            else:
+                em_group_inventory = ()
+                em_source_arrays_sorted = sorted(em_source_arrays.items(), key=lambda kv: np.prod(kv[1].shape), reverse=True)
+                gt_source_arrays_sorted = sorted(crop_multiscale_group.items(), key=lambda kv: np.prod(kv[1].shape), reverse=True)
+                
+                # apply padding in a resolution-aware way
+                _, (base_em_scale, _) = transforms_from_coords(em_source_arrays_sorted[0][1].coords, transform_precision=4)
+                _, (base_gt_scale, _) = transforms_from_coords(gt_source_arrays_sorted[0][1].coords, transform_precision=4)
 
-            log.info(f'Preparing to fetch {len(em_group_inventory)} files from {em_source_url}.')
-            partition_copy_store(
-                keys=em_group_inventory, 
-                source_store=em_source_group.store, 
-                dest_store=em_dest_group.store, 
-                batch_size=256, 
-                pool=pool,
-                log=log)
+                for key, array in em_source_arrays_sorted:
+                    _, (current_scale, _) = transforms_from_coords(array.coords, transform_precision=4)
+                    if fetch_all_em_resolutions: ratio_threshold = 0
+                    else: ratio_threshold = 0.9
+                    scale_ratios = tuple(s_current / s_gt for s_current, s_gt in zip(current_scale.scale, base_gt_scale.scale))
+                    if all(tuple(x > ratio_threshold for x in scale_ratios)):
+                        
+                        relative_scale = base_em_scale.scale[0] / current_scale.scale[0]
+                        current_pad = int(padding * relative_scale)
+                        slices = subset_to_slice(array, crop_multiscale_group['s0'])
+                        slices_padded = tuple(slice(max(sl.start - current_pad, 0), min(sl.stop + current_pad, shape), sl.step) for sl, shape in zip(slices, array.shape))
+                        new_chunks = tuple(map(lambda v: '/'.join([key, v]), get_chunk_keys(em_source_group[key], slices_padded)))
+                        log.info(f'Gathering {len(new_chunks)} chunks from level {key}.')
+                        em_group_inventory += new_chunks
+                    else:
+                        log.info(f'Skipping scale level {key} because it is sampled more densely than the groundtruth data')
+
+                log.info(f'Preparing to fetch {len(em_group_inventory)} files from {em_source_url}.')
+                partition_copy_store(
+                    keys=em_group_inventory, 
+                    source_store=em_source_group.store, 
+                    dest_store=em_dest_group.store, 
+                    batch_size=256, 
+                    pool=pool,
+                    log=log)
 
     log = log.unbind('crop')
     log.info(f'Done after {time.time() - fetch_save_start:0.3f}s')
