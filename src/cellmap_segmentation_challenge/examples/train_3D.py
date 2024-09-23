c2506f9a37420dc85b7dde4fea476d7df7aa7a17
rhoadesScholar <rhoadesj@hhmi.org>
2024-09-23 17:38:18 -0400

    feat(Prediction): 🚧 Prediction functions in place. Ready for debug.

--- src/cellmap_segmentation_challenge/examples/train_3D.py
+++ src/cellmap_segmentation_challenge/examples/train_3D.py
@@ -72,18 +72,6 @@ np.random.seed(random_seed)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Training device: {device}")
 
-# %% Download the data and make the dataloader
-train_loader, val_loader = get_dataloader(
-    datasplit_path,
-    classes,
-    batch_size=batch_size,
-    input_array_info=input_array_info,
-    target_array_info=target_array_info,
-    spatial_transforms=spatial_transforms,
-    iterations_per_epoch=iterations_per_epoch,
-    device=device,
-)
-
 # %% Move model to device
 model = model.to(device)
 
@@ -100,98 +88,113 @@ load_latest(model_save_path.format(epoch="*", model_name=model_to_load), model)
 # )
 
 
-# %% Define the optimizer
-optimizer = torch.optim.RAdam(model.parameters(), lr=learning_rate)
-
-# %% Define the loss function
-criterion = torch.nn.BCEWithLogitsLoss
-
-# Use custom loss function wrapper that handles NaN values in the target. This works with any PyTorch loss function
-criterion = CellMapLossWrapper(criterion)
-
-# %% Train the model
-post_fix_dict = {}
-
-# Define a summarywriter
-writer = SummaryWriter(logs_save_path.format(model_name=model_name))
-
-# Create a variable to track iterations
-n_iter = 0
-# Training outer loop, across epochs
-for epoch in range(epochs):
-
-    # Set the model to training mode to enable backpropagation
-    model.train()
-
-    # Training loop for the epoch
-    post_fix_dict["Epoch"] = epoch + 1
-    epoch_bar = tqdm(train_loader.loader, desc="Training")
-    for batch in epoch_bar:
-        # Increment the training iteration
-        n_iter += 1
-
-        # Get the inputs and targets
-        inputs = batch["input"]
-        targets = batch["output"]
-
-        # Zero the gradients, so that they don't accumulate across iterations
-        optimizer.zero_grad()
-
-        # Forward pass (compute the output of the model)
-        outputs = model(inputs)
-
-        # Compute the loss
-        loss = criterion(outputs, targets)
-
-        # Backward pass (compute the gradients)
-        loss.backward()
-
-        # Update the weights
-        optimizer.step()
-
-        # Update the progress bar
-        post_fix_dict["Loss"] = f"{loss.item()}"
-        epoch_bar.set_postfix(post_fix_dict)
-
-        # Log the loss using tensorboard
-        writer.add_scalar("loss", loss.item(), n_iter)
-
-    # Save the model
-    torch.save(
-        model.state_dict(),
-        model_save_path.format(epoch=epoch + 1, model_name=model_name),
+def train():
+    # %% Download the data and make the dataloader
+    train_loader, val_loader = get_dataloader(
+        datasplit_path,
+        classes,
+        batch_size=batch_size,
+        input_array_info=input_array_info,
+        target_array_info=target_array_info,
+        spatial_transforms=spatial_transforms,
+        iterations_per_epoch=iterations_per_epoch,
+        device=device,
     )
 
-    # Set the model to evaluation mode to disable backpropagation
-    model.eval()
+    # %% Define the optimizer
+    optimizer = torch.optim.RAdam(model.parameters(), lr=learning_rate)
 
-    # Compute the validation score by averaging the loss across the validation set
-    val_score = 0
-    val_bar = tqdm(val_loader, desc="Validation")
-    with torch.no_grad():
-        for batch in val_bar:
+    # %% Define the loss function
+    criterion = torch.nn.BCEWithLogitsLoss
+
+    # Use custom loss function wrapper that handles NaN values in the target. This works with any PyTorch loss function
+    criterion = CellMapLossWrapper(criterion)
+
+    # %% Train the model
+    post_fix_dict = {}
+
+    # Define a summarywriter
+    writer = SummaryWriter(logs_save_path.format(model_name=model_name))
+
+    # Create a variable to track iterations
+    n_iter = 0
+    # Training outer loop, across epochs
+    for epoch in range(epochs):
+
+        # Set the model to training mode to enable backpropagation
+        model.train()
+
+        # Training loop for the epoch
+        post_fix_dict["Epoch"] = epoch + 1
+        epoch_bar = tqdm(train_loader.loader, desc="Training")
+        for batch in epoch_bar:
+            # Increment the training iteration
+            n_iter += 1
+
+            # Get the inputs and targets
             inputs = batch["input"]
             targets = batch["output"]
+
+            # Zero the gradients, so that they don't accumulate across iterations
+            optimizer.zero_grad()
+
+            # Forward pass (compute the output of the model)
             outputs = model(inputs)
+
+            # Compute the loss
             loss = criterion(outputs, targets)
-            val_score += loss.item()
 
-    val_score /= len(val_loader)
-    # Log the validation using tensorboard
-    writer.add_scalar("validation", val_score, n_iter)
+            # Backward pass (compute the gradients)
+            loss.backward()
 
-    # Update the progress bar
-    post_fix_dict["Validation"] = f"{val_score:.4f}"
+            # Update the weights
+            optimizer.step()
 
-    # Generate and save figures from the last batch of the validation to appear in tensorboard
-    figs = get_image_dict(inputs, targets, outputs, classes)
-    for name, fig in figs.items():
-        writer.add_figure(name, fig, n_iter)
+            # Update the progress bar
+            post_fix_dict["Loss"] = f"{loss.item()}"
+            epoch_bar.set_postfix(post_fix_dict)
 
-    # Refresh the train loader to shuffle the data yielded by the dataloader
-    train_loader.refresh()
+            # Log the loss using tensorboard
+            writer.add_scalar("loss", loss.item(), n_iter)
 
-# Close the summarywriter
-writer.close()
+        # Save the model
+        torch.save(
+            model.state_dict(),
+            model_save_path.format(epoch=epoch + 1, model_name=model_name),
+        )
 
-# %%
+        # Set the model to evaluation mode to disable backpropagation
+        model.eval()
+
+        # Compute the validation score by averaging the loss across the validation set
+        val_score = 0
+        val_bar = tqdm(val_loader, desc="Validation")
+        with torch.no_grad():
+            for batch in val_bar:
+                inputs = batch["input"]
+                targets = batch["output"]
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                val_score += loss.item()
+
+        val_score /= len(val_loader)
+        # Log the validation using tensorboard
+        writer.add_scalar("validation", val_score, n_iter)
+
+        # Update the progress bar
+        post_fix_dict["Validation"] = f"{val_score:.4f}"
+
+        # Generate and save figures from the last batch of the validation to appear in tensorboard
+        figs = get_image_dict(inputs, targets, outputs, classes)
+        for name, fig in figs.items():
+            writer.add_figure(name, fig, n_iter)
+
+        # Refresh the train loader to shuffle the data yielded by the dataloader
+        train_loader.refresh()
+
+    # Close the summarywriter
+    writer.close()
+
+
+if __name__ == "__main__":
+    train()
