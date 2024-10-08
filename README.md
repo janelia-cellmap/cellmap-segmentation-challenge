be1d634b9fb236d776b5f27e1f74b0ff97331c2a
rhoadesScholar <rhoadesj@hhmi.org>
2024-10-08 13:53:27 -0400

    Update README.md and remove unused files

--- README.md
+++ README.md
@@ -40,7 +40,37 @@ pip install -e .
 
 The repository is structured as follows:
 
-...
+```
+
+cellmap-segmentation-challenge/
+│
+├── examples/
+│   ├── train_2d.py
+│   ├── train_3d.py
+│   ├── predict_2d.py
+│   ├── predict_3d.py
+│   ├── ...
+│   └── README.md
+│
+├── src/
+│   ├── cellmap_segmentation_challenge/
+│   │   ├── models/
+│   │   │   ├── model_load.py
+│   │   │   ├── unet_model_2D.py
+│   │   │   ├── ...
+│   │   │   └── README.md
+│   │   └── ...
+│
+├── data/
+│   ├── ...
+│   └── README.md
+│
+├── ...
+│
+├── README.md
+└── ...
+```
+
 
 
 ## Download the data
@@ -71,21 +101,51 @@ You can also train a 3D model by running the same command with `train_3d.py`:
 python train_3d.py
 ```
 
-For more information on the available options, see the `README.md` in the examples folder (`src/cellmap_segmentation_challenge/examples`), as well as the documentation in `src/cellmap_segmentation_challenge/examples/train_2d.py` and `src/cellmap_segmentation_challenge/examples/train_3d.py`.
+For more information on the available options, see the `README.md` in the `examples` folder, as well as the documentation in `examples/train_2d.py` and `examples/train_3d.py`.
 
 ## Predict on test data
 
-You can predict on test data by running the following command:
+Example scripts for predicting on test data are provided in the `examples` directory. The scripts are named `predict_2D.py` and `predict_3D.py`, respectively, and are annotated for clarity. You can run one such script by simply running the following on the command line:
 
 ```bash
-...
+python predict_2D.py
 ```
+
+See the `README.md` in the `examples` folder for more information on the available options, as well as the documentation in `examples/predict_2D.py` and `examples/predict_3D.py`.
 
 ## Submit your predictions
 
-To submit your predictions, first make sure that they are in the correct format, then submit them through [the online submission portal](https://staging.cellmapchallenge.2i2c.cloud/upload). You will need to sign in with your GitHub account to submit your predictions.
+To submit your predictions, first make sure that they are in the correct format (see below), then submit them through [the online submission portal](https://staging.cellmapchallenge.2i2c.cloud/upload). You will need to sign in with your GitHub account to submit your predictions.
 
-To make sure your predictions are in the correct format, ...
+Submission file format requirements:
+1. The submission should be a single zip file containing a single Zarr-2 file with the following structure:
+```
+submission.zarr
+    - /<test_volume_name>
+    - /<label_name>
+```
+2. The names of the test volumes and labels should match the names of the test volumes and labels in the test data. See `examples/predict_2D.py` and `examples/predict_3D.py` for examples of how to generate predictions in the correct format.
+3. The scale for all volumes is 8x8x8 nm/voxel, except as otherwise specified.
 
+Assuming your data is already 8x8x8nm/voxel,and each label volume is either A) a 3D binary volume with the same shape and scale as the corresponding test volume, 
+or B) instance IDs per object, you can convert the submission to the required format using the following convenience functions:
+
+- For converting a single 3D numpy array of class labels to a Zarr-2 file, use the following function:
+  `cellmap_segmentation_challenge.utils.evaluate.save_numpy_labels_to_zarr`
+Note: The class labels should start from 1, with 0 as background.
+
+- For converting a list of 3D numpy arrays of binary or instance labels to a Zarr-2 file, use the following function:
+  `cellmap_segmentation_challenge.utils.evaluate.save_numpy_binary_to_zarr`
+Note: The instance labels, if used, should be unique IDs per object, with 0 as background.
+
+The arguments for both functions are the same:
+- `submission_path`: The path to save the Zarr-2 file (ending with <filename>.zarr).
+- `test_volume_name`: The name of the test volume.
+- `label_names`: A list of label names corresponding to the list of 3D numpy arrays or the number of the class labels (0 is always assumed to be background).
+- `labels`: A list of 3D numpy arrays of binary labels or a single 3D numpy array of class labels.
+- `overwrite`: A boolean flag to overwrite the Zarr-2 file if it already exists.
+
+To zip the Zarr-2 file, you can use the following command:    
 ```bash
-...
+zip -r submission.zip submission.zarr
+```
--- src/cellmap_segmentation_challenge/benchmark/evaluate.py
+++ src/cellmap_segmentation_challenge/benchmark/evaluate.py
@@ -1,41 +1,23 @@
 # %%
-"""
-Add tolerance based on known GT precision --> mask out uncertain pixels (distance transform then threshold)
-Upsample GT to match prediction resolution --> should be native resolution (8nm/)
-Determine tolerance per label --> Aubrey
-Combined metric:
-- geometric mean of Jaccard score and Hausdorff distance
-
-For semantic:
-- Jaccard score
-- Hausdorff distance w/ cutoff or sigmoid
-
-For instance:
-- Jaccard score
---> Match by best Jaccard score
-    - False positive = unmatched pred
-    - False negative = unmatched GT
-    - Count false splits
-        - near matches over a threshold are incorrect splits where the GT is split into multiple predictions
-    - Count false merges
-        - near matches over a threshold are incorrect merges where multiple GT are merged into a single prediction
-- Hausdorff distance w/ cutoff or sigmoid
-
-"""
-# %%
 import numpy as np
 import gunpowder as gp
 from skimage.measure import label
 import matplotlib.pyplot as plt
 
-from cellmap_segmentation_challenge.benchmark.pipeline import random_source_pipeline, simulate_predictions_accuracy, simulate_predictions_iou
+from cellmap_segmentation_challenge.benchmark.pipeline import (
+    random_source_pipeline,
+    simulate_predictions_accuracy,
+    simulate_predictions_iou,
+)
 from cellmap_segmentation_challenge.evaluate import *
 
+show = True
 
 # %% First make a GT array
 
 # SET SHAPE, NUMBER OF POINTS, ETC. HERE
-shape = (128, 128, 128)  # (z, y, x) shape of the volume
+size = 512
+shape = (size, size, size)  # (z, y, x) shape of the volume
 num_points = [
     shape[0] // 10,
     shape[0] // 5,
@@ -49,42 +31,50 @@ src, req = random_source_pipeline(
     shape=shape, num_points=num_points, relabel_connectivity=len(shape)
 )
 
+print(f"Building synthetic volume with shape: {shape}")
+
 with gp.build(src):
     batch = src.request_batch(req)
     truth_label = list(batch.arrays.values())[0].data
     truth_label = label(truth_label, connectivity=len(shape))
 
 # %%
-# Plot the GT array and print some info
-plt.imshow(truth_label[shape[0] // 2], cmap="tab20", interpolation="nearest")
-plt.colorbar()
+if show:
+    # Plot the GT array and print some info
+    plt.imshow(truth_label[shape[0] // 2], cmap="tab20", interpolation="nearest")
+    plt.colorbar()
 
-print(f"Shape of volume: {truth_label.shape}\nNumber of IDs: {len(np.unique(truth_label))}")
+print(
+    f"Shape of volume: {truth_label.shape}\nNumber of IDs: {len(np.unique(truth_label))}"
+)
 
 # %%
 # Make a prediction array for iou testing
 pred_iou_label = simulate_predictions_iou(truth_label, configured_iou)
 
 # Plot the prediction array
-plt.imshow(pred_iou_label[shape[0] // 2], cmap="tab20", interpolation="nearest")
-plt.colorbar()
+if show:
+    plt.imshow(pred_iou_label[shape[0] // 2], cmap="tab20", interpolation="nearest")
+    plt.colorbar()
 
-print(f"Configured iou: {configured_iou}\nShape of volume: {pred_iou_label.shape}\nNumber of IDs: {len(np.unique(pred_iou_label))}")
+print(
+    f"Configured iou: {configured_iou}\nShape of volume: {pred_iou_label.shape}\nNumber of IDs: {len(np.unique(pred_iou_label))}"
+)
 
-#%%
+# %%
 print("Semantic scoring:")
 semantic_score = score_semantic(pred_iou_label, truth_label)
 print(semantic_score)
 print(f"Configured IOU: {configured_iou}")
 
-# %%
-print("Timing semantic scoring:")
-semantic_time = %timeit -o score_semantic(pred_iou_label, truth_label)
-normalized_semantic_time = semantic_time.average / np.prod(shape)
-print(f"Normalized time for semantic scoring: {normalized_semantic_time} seconds per voxel")
+# # %%
+# if show:
+#     print("Timing semantic scoring:")
+#     semantic_time = %timeit -o score_semantic(pred_iou_label, truth_label)
+#     normalized_semantic_time = semantic_time.average / np.prod(shape)
+#     print(f"Normalized time for semantic scoring: {normalized_semantic_time} seconds per voxel")
 
-size = 512
-print(f"Estimate for {size}^3 volume: {normalized_semantic_time * size**3} seconds")
+#     print(f"Estimate for {size}^3 volume: {normalized_semantic_time * size**3} seconds")
 
 
 # %%
@@ -92,10 +82,15 @@ print(f"Estimate for {size}^3 volume: {normalized_semantic_time * size**3} secon
 pred_accuracy_label = simulate_predictions_accuracy(truth_label, configured_accuracy)
 
 # Plot the prediction array
-plt.imshow(pred_accuracy_label[shape[0] // 2], cmap="tab20", interpolation="nearest")
-plt.colorbar()
+if show:
+    plt.imshow(
+        pred_accuracy_label[shape[0] // 2], cmap="tab20", interpolation="nearest"
+    )
+    plt.colorbar()
 
-print(f"Configured accuracy: {configured_accuracy}\nShape of volume: {pred_accuracy_label.shape}\nNumber of IDs: {len(np.unique(pred_accuracy_label))}")
+print(
+    f"Configured accuracy: {configured_accuracy}\nShape of volume: {pred_accuracy_label.shape}\nNumber of IDs: {len(np.unique(pred_accuracy_label))}"
+)
 
 
 # %%
@@ -105,13 +100,13 @@ print(instance_score)
 print(f"Configured accuracy: {configured_accuracy}")
 
 # %%
-print("Timing instance scoring:")
-instance_time = %timeit -o score_instance(pred_accuracy_label, truth_label)
-normalized_instance_time = instance_time.average / np.prod(shape)
-print(f"Normalized time for instance scoring: {normalized_instance_time} seconds per voxel")
+# if show:
+#     print("Timing instance scoring:")
+#     instance_time = %timeit -o score_instance(pred_accuracy_label, truth_label)
+#     normalized_instance_time = instance_time.average / np.prod(shape)
+#     print(f"Normalized time for instance scoring: {normalized_instance_time} seconds per voxel")
 
-size = 512
-print(f"Estimate for {size}^3 volume: {normalized_instance_time * size**3} seconds")
+#     print(f"Estimate for {size}^3 volume: {normalized_instance_time * size**3} seconds")
 # %%
 
 # Now let's test the whole pipeline by first saving the GT and prediction arrays to disk using the included utility functions
@@ -132,7 +127,9 @@ pred_path = "pred.zarr"
 pred_array = [pred_accuracy_label, pred_iou_label]
 pred_names = ["instance", "semantic"]
 
-save_numpy_class_arrays_to_zarr(pred_path, "test", pred_names, pred_array, overwrite=True)
+save_numpy_class_arrays_to_zarr(
+    pred_path, "test", pred_names, pred_array, overwrite=True
+)
 
 # Now zip it
 os.system(f"zip -r {pred_path.replace('zarr', 'zip')} {pred_path}")
@@ -140,9 +137,12 @@ os.system(f"zip -r {pred_path.replace('zarr', 'zip')} {pred_path}")
 # %%
 # Now we can score the saved arrays
 from cellmap_segmentation_challenge.evaluate import score_submission
+
 save_path = "scores.json"
 submission_path = "pred.zip"
 truth_path = "gt.zarr"
 
-score_submission(submission_path, save_path, truth_path=truth_path, instance_classes=["instance"])
+score_submission(
+    submission_path, save_path, truth_path=truth_path, instance_classes=["instance"]
+)
 # %%
